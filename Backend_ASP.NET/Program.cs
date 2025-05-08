658b07c5ced613272ef30dc23b30a98d6953e7ff
PhanHoangTrieu <phanhoangtrieunk@gmail.com>
2025-05-08 12:29:17 +0700

    update

--- Backend_ASP.NET/Program.cs
+++ Backend_ASP.NET/Program.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 // xuat log ra teminal
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
